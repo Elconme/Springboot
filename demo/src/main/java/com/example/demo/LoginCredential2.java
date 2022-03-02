d87d7cbad498b023594f15ce36c1dfd6323a783f
Elconme <201910743@alu.comillas.edu>
2022-03-02 13:45:01 +0100

    springboot

---
+++ demo/src/main/java/com/example/demo/LoginCredential2.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import javax.validation.constraints.Pattern;
+
+
+record LoginCredential2(
+
+    @Pattern(message="max 5 words please" , regexp="^[a-zA-Z-.0-9]{1,5}$")
+    String user,
+
+    @Pattern(message="max 5 words please" , regexp="^[a-zA-Z-.0-9]{1,5}$")
+    String password) {}
+
